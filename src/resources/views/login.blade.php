03ae7f8bb90d5d58a71a3d9d6057ae3fc08e40fc
manami <utau.3152@outlook.>
2025-02-25 21:30:26 +0900

    バリエーション前

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -6,25 +6,33 @@
 
 @section('content')
 
-<form class="form" action="/register" method="get">
+<form class="form" action="/register" method="get" >
     <button class="register__button-submit" type="submit">register</button>
-
 
 <div class="login__content">
     <div class="login__heading">
         <h2>Login</h2>
     </div>
 
-    <form class="form" action="/login" method="get">
+    <form class="form" action="/login" method="post">
+        @csrf
         <div class="login__group">
             <div class="login__group-title">
                 <span class="login__label--item">メールアドレス</span>
-                <input class="text" mane="email" placeholder="例：test@example.com"/>
-                <div class="login__group">
+                <input class="text" type="email" mane="email" placeholder="例：test@example.com"/>
+            </div>
+        </div>
+        <div class="login__group">
             <div class="login__group-title">
                 <span class="login__label--item">パスワード</span>
-                <input class="text" mane="name" placeholder="例：coachtech1106"/>
-                <div class="login__button">
+                <input class="text" type="password" mane="name" placeholder="例：coachtech1106"/>
+            </div>
+        </div>
+    </form>
+    <form class="form" action="/admin" method="get">
+        <div class="login__button">
             <button class="login__button-submit" type="submit">ログイン</button>
         </div>
+    </form>
+</div>
 @endsection
